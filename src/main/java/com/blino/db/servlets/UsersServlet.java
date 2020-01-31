4355332b49bc09f237c0d2b1ef1401d38fce8c05
Roman Blinov <romanblinov1613@gmail.com>
2020-01-31 22:33:54 +0300

    servlet with initialization

---
+++ src/main/java/com/blino/db/servlets/UsersServlet.java
@@ -0,0 +1,62 @@
+package com.blino.db.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+
+@WebServlet("/users")
+public class UsersServlet extends HttpServlet {
+
+    private Connection connection;
+    // Вся инициализация прописана в методе init
+    @Override
+    public void init() throws ServletException {
+        Properties properties = new Properties();
+        try {
+            properties.load(new FileInputStream(getServletContext().getRealPath("/WEB-INF/classes/db.properties")));
+//            System.out.println(properties.getProperty("db.url"));
+            String dbUrl = properties.getProperty("db.url");
+            String dbUsername = properties.getProperty("db.username");
+            String dbPassword = properties.getProperty("db.password");
+            String dbDriverClassName = properties.getProperty("db.driverClassName");
+
+            Class.forName(dbDriverClassName);
+
+            connection = DriverManager.getConnection(dbUrl, dbUsername, dbPassword);
+
+        } catch (IOException  | SQLException | ClassNotFoundException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.getServletContext().getRequestDispatcher("/jsp/addUser.jsp").forward(req, resp);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        String firstName = req.getParameter("first-name");
+        String lastName = req.getParameter("last-name");
+
+        try {
+            Statement statement = connection.createStatement();
+            String sqlInsert = "INSERT INTO fix_user(first_name, last_name) " +
+                    "VALUES('" + firstName + "', '" + lastName + "');";
+            System.out.println(sqlInsert);
+            statement.execute(sqlInsert);
+
+        } catch (SQLException e) {
+            throw new IllegalStateException(e);
+        }
+    }
+}
